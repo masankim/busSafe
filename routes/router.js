cb346df93fd344a85e1a300ab8e7e99eff8552fc
GaryKim <moduedu@gmail.com>
2020-12-12 18:01:33 +0900

    busSafe Dapp 완성

--- routes/router.js
+++ routes/router.js
@@ -396,4 +396,200 @@ router.get("/m_checklist_detail1/:car_id", function (req, res) {
       );
     }
   });
+
+router.get("/m_car_info/:car_id", function (req, res) {
+  if (!req.session.loggedIn) {
+    res.redirect("/logout");
+  } else {
+    const carnum = req.params.car_id;
+    connection.query(
+      "SELECT * FROM car_list where car_id = ?",
+      [carnum],
+      function (err, rows, fields) {
+        if (err) {
+          console.log("query is not excuted. select fail...\n" + err);
+        } else {
+          connection.query(
+            "SELECT name, division FROM user_list where post_id = ?",
+            [req.session.loggedIn.post_id],
+            function (err, result, fields) {
+              if (err) {
+                console.log(err);
+              } else {
+                res.render("m_car_info", {
+                  carlist: rows,
+                  userlist: result,
+                  user: req.session.loggedIn,
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+router.get("/m_add_car_update/:car_id", function (req, res) {
+  if (!req.session.loggedIn) {
+    res.redirect("/logout");
+  } else {
+    const carnum = req.params.car_id;
+    connection.query(
+      "SELECT * FROM car_list where car_id = ?",
+      [carnum],
+      function (err, rows, fields) {
+        if (err) {
+          console.log("query is not excuted. select fail...\n" + err);
+        } else {
+          connection.query(
+            "SELECT name, division FROM user_list where post_id = ?",
+            [req.session.loggedIn.post_id],
+            function (err, result, fields) {
+              if (err) {
+                console.log(err);
+              } else {
+                res.render("m_add_car_update", {
+                  carlist: rows,
+                  userlist: result,
+                  user: req.session.loggedIn,
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+router.post("/m_add_car_update", function (req, res) {
+  if (!req.session.loggedIn) {
+    res.redirect("/logout");
+  } else {
+    connection.query(
+      `select * from user_list where post_id = ?`,
+      [req.session.loggedIn.post_id],
+      function (err, result) {
+        if (err) {
+          res.render("error");
+        } else {
+          const car_id = req.body.car_id;
+          const car_div = req.body.car_div;
+          const car_type = req.body.car_type;
+          const car_birth = req.body.car_birth;
+          const car_day = req.body.car_day;
+          const car_result = req.body.car_result;
+
+          connection.query(
+            "UPDATE car_list SET car_id = ?, car_div = ?, car_type = ?, car_birth = ?, car_day = ?, car_result = ? where car_id = ?",
+            [car_id, car_div, car_type, car_birth, car_day, car_result, car_id],
+            function (err, rows, next) {
+              if (err) {
+                console.log(err);
+              } else {
+                res.redirect("/m_car_list");
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+router.get("/m_add_car", function (req, res) {
+  if (!req.session.loggedIn) {
+    res.redirect("/logout");
+  } else {
+    var sql = "SELECT * FROM car_list";
+    connection.query(sql, function (err, rows, fields) {
+      if (err) {
+        console.log("query is not excuted. select fail...\n" + err);
+      } else {
+        connection.query(
+          "SELECT * FROM user_list where post_id = ?",
+          [req.session.loggedIn.post_id],
+          function (err, result, fields) {
+            if (err) {
+              console.log(err);
+            } else {
+              res.render("m_add_car", {
+                carlist: rows,
+                userlist: result,
+                user: req.session.loggedIn,
+              });
+            }
+          }
+        );
+      }
+    });
+  }
+});
+router.post("/m_add_car", function (req, res) {
+  const car_id = req.body.car_id;
+  const car_div = req.body.car_div;
+  const car_type = req.body.car_type;
+  const car_birth = req.body.car_birth;
+  const car_day = req.body.car_day;
+  const car_result = req.body.var_result;
+  console.log(car_id, car_div, car_type, car_birth, car_day, car_result);
+
+  if (!req.session.loggedIn) {
+    res.redirect("/logout");
+  } else {
+    connection.query(
+      `INSERT INTO car_list (car_id, car_div, car_type, car_birth, car_day, car_result) values (?, ?, ?, ?, ?, ?)`,
+      [car_id, car_div, car_type, car_birth, car_day, car_result],
+      function (err, result) {
+        if (err) {
+          console.log("error");
+        } else {
+          res.redirect("m_car_list");
+        }
+      }
+    );
+  }
+});
+
+router.get("/m_car_info/delete/:num", function (req, res) {
+  if (!req.session.loggedIn) {
+    res.redirect("/logout");
+  } else {
+    const carnum = req.params.num;
+    connection.query(
+      `delete from car_list where car_id = ?`,
+      [carnum],
+      function (err, result) {
+        if (err) {
+          console.log(err);
+          res.render("error");
+        } else {
+          var sql = "SELECT * FROM car_list";
+          connection.query(sql, function (err, rows, fields) {
+            if (err) {
+              console.log("query is not excuted. select fail...\n" + err);
+            } else {
+              connection.query(
+                "SELECT name, post_id FROM user_list where post_id = ?",
+                [req.session.loggedIn.post_id],
+                function (err, result, fields) {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    res.render("m_Car_list", {
+                      carlist: rows,
+                      userlist: result,
+                      user: req.session.loggedIn,
+                    });
+                  }
+                }
+              );
+            }
+          });
+        }
+      }
+    );
+  }
+});
 module.exports= router;
